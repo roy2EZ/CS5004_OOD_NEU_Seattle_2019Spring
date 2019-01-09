51fb068835fc503a70c762326ceaf2cdfa2d7985
Roy Chen <chen.rongy@husky.neu.edu>
2019-01-08 20:01:55 -0800

    lab1 Author class update

---
+++ lab1/src/AuthorTest.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+public class AuthorTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+    @org.junit.Test
+    public void getName() throws Exception {
+        TestCase.fail("Not yet implemented");
+    }
+    @org.junit.Test
+    public void getEmail() throws Exception	{
+        TestCase.fail("Not yet implemented");
+    }
+    @org.junit.Test
+    public void getAddress() throws Exception {
+        TestCase.fail("Not yet implemented");
+    }
+}
