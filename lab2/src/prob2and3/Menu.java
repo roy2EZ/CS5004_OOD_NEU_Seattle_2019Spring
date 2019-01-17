b0416aa06b665744592fd85ca300c7fb21ef27dd
Roy Chen <chen.rongy@husky.neu.edu>
2019-01-17 18:13:46 -0500

    update lab2

--- lab2/src/prob2and3/Menu.java
+++ lab2/src/prob2and3/Menu.java
@@ -1,11 +1,25 @@
+
 package prob2and3;
 
+/**
+ *  Represents a menu with their details--meals, desserts, beverages and drinks
+ *
+ * @author roych
+ * @version 1.0
+ */
 public class Menu {
   private String meals;
   private String desserts;
   private String beverages;
   private String drinks;
 
+  /**
+   * Creates a new menu given the menu's meals, desserts, beverages, and drinks
+   * @param meals the menu's meals
+   * @param desserts the menu's desserts
+   * @param beverages the menu's beverages
+   * @param drinks the menu's drinks
+   */
   public Menu(String meals, String desserts, String beverages, String drinks) {
     this.meals = meals;
     this.desserts = desserts;
@@ -13,34 +27,67 @@ public class Menu {
     this.drinks = drinks;
   }
 
+  /**
+   *
+   * @return the meals
+   */
+
   public String getMeals() {
     return meals;
   }
 
+  /**
+   *
+   * @param meals set the meals
+   */
   public void setMeals(String meals) {
     this.meals = meals;
   }
 
+  /**
+   *
+   * @return the desserts
+   */
   public String getDesserts() {
     return desserts;
   }
 
+  /**
+   *
+   * @param desserts set the desserts
+   */
   public void setDesserts(String desserts) {
     this.desserts = desserts;
   }
 
+  /**
+   *
+   * @return the beverages
+   */
   public String getBeverages() {
     return beverages;
   }
 
+  /**
+   *
+   * @param beverages set the beverages
+   */
   public void setBeverages(String beverages) {
     this.beverages = beverages;
   }
 
+  /**
+   *
+   * @return the drinks
+   */
   public String getDrinks() {
     return drinks;
   }
 
+  /**
+   *
+   * @param drinks set the drinks
+   */
   public void setDrinks(String drinks) {
     this.drinks = drinks;
   }
--- lab2/src/prob2and3/Restaurant.java
+++ lab2/src/prob2and3/Restaurant.java
@@ -1,10 +1,29 @@
 package prob2and3;
 
+/**
+ *
+ * Represents a Restaurant with their details--name, address,
+ * is the restaurant open or not and menu.
+ *
+ * @author roych
+ * @version 1.0
+ */
 public class Restaurant {
   private String name;
   private String address;
   private Boolean isOpen;
   private Menu menu;
+
+  /**
+   *
+   * Creates a new Restaurant given the name, address as strings,
+   * isOpen as boolean and menu as a Menu class.
+   *
+   * @param name the restaurant's name
+   * @param address the restaurant's address
+   * @param isOpen if the restaurant is open or closed, true for open, false for closed
+   * @param menu the menu
+   */
 
   public Restaurant(String name, String address, Boolean isOpen, Menu menu) {
     this.name = name;
@@ -13,30 +32,58 @@ public class Restaurant {
     this.menu = menu;
   }
 
+  /**
+   *
+   * @return the name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   *
+   * @param name
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   *
+   * @return the address
+   */
   public String getAddress() {
     return address;
   }
 
+  /**
+   *
+   * @param address
+   */
   public void setAddress(String address) {
     this.address = address;
   }
 
+  /**
+   *
+   * @return the isOpen, true for open, false for closed
+   */
   public Boolean getOpen() {
     return isOpen;
   }
 
+  /**
+   *
+   * @param open
+   */
   public void setOpen(Boolean open) {
     isOpen = open;
   }
 
+  /**
+   *
+   * @return the menu
+   */
   public Menu getMenu() {
     return this.menu;
   }
