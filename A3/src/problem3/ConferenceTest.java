7f1dd0af2e8fa86e8372676d5c9d5ed69711eb9c
Roy Chen <chen.rongy@husky.neu.edu>
2019-02-03 19:48:50 -0800

    Create ConferenceTest.java
    create conference test

---
+++ A3/src/problem3/ConferenceTest.java
@@ -0,0 +1,47 @@
+package problem3;
+
+import static org.junit.Assert.*;
+
+import java.time.Year;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConferenceTest {
+  private Conference testConference;
+
+  @Before
+  public void setUp() throws Exception {
+    testConference = new Conference("testTitle", "testAuthor", "testConferenceName",
+        "Seattle, WA", "Jul", Year.of(2015));
+  }
+
+  @Test
+  public void getTitle() {
+    assertEquals("testTitle", testConference.getTitle());
+  }
+
+  @Test
+  public void getAuthor() {
+    assertEquals("testAuthor", testConference.getAuthor());
+  }
+
+  @Test
+  public void getConferenceName() {
+    assertEquals("testConferenceName", testConference.getConferenceName());
+  }
+
+  @Test
+  public void getLocation() {
+    assertEquals("Seattle, WA", testConference.getLocation());
+  }
+
+  @Test
+  public void getMonth() {
+    assertEquals("Jul", testConference.getMonth());
+  }
+
+  @Test
+  public void getYear() {
+    assertEquals(2015, testConference.getYear().getValue());
+  }
+}
