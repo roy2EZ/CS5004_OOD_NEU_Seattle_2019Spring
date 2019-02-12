acea15f9ff828b887543307c6369d846f998d6c0
Roy Chen <chen.rongy@husky.neu.edu>
2019-02-12 15:24:48 -0800

    create a4 prob1
    - updated pom.xml
    - create needed enums

---
+++ Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/Disabilities.java
@@ -0,0 +1,10 @@
+package edu.neu.ccs.cs5004.problem1;
+
+/**
+ * represents the disabilities of a vehicle or a parking lot
+ * @author Roy Chen
+ * @version 1.0
+ */
+public enum Disabilities {
+  REGULAR, DESABILITY
+}
---
+++ Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/Size.java
@@ -0,0 +1,10 @@
+package edu.neu.ccs.cs5004.problem1;
+
+/**
+ * Represents the size of an vehicle or parking lot
+ * @author Roy Chen
+ * @version 1.0
+ */
+public enum Size {
+  SMALL, MEDUIM, LARGE
+}
