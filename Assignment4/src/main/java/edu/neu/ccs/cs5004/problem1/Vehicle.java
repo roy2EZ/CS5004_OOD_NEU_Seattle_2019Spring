4a5dbdc463682cca2721f5c3b5487f4789072eb8
Roy Chen <chen.rongy@husky.neu.edu>
2019-02-13 00:27:18 -0800

    Revert "update a4 prob1"
    This reverts commit 03f0e045509b5070e943d81bc73841eb605bcaa9.

--- Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package edu.neu.ccs.cs5004.problem1;
-
-public class Vehicle extends AbstractVehicle {
-
-  public Vehicle(Size vehicleSize, Disabilities vehicleDisType) {
-    super(vehicleSize, vehicleDisType);
-  }
-}
