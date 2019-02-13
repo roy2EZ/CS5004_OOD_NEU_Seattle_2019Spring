e25c829ff7de3e41ed107e5b957b53aca6553b71
Roy Chen <chen.rongy@husky.neu.edu>
2019-02-13 00:41:42 -0800

    Revert "update a4 prob1"
    This reverts commit 03f0e045509b5070e943d81bc73841eb605bcaa9.

--- Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/IParkingSpot.java
+++ Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/IParkingSpot.java
@@ -7,16 +7,18 @@ public interface IParkingSpot {
    * @param parkingSpot the parking spot
    * @return returns true if a vehicle was successfully parked, and false otherwise
 */
-  Boolean parkVehicle(Vehicle vehicle, ParkingSpot parkingSpot);
+  Boolean parkVehicle(AbstractVehicle vehicle, IParkingSpot parkingSpot);
 
   /**
    * @param vehicle the vehicle which is looking for parking spot
+   * @param parkingSpot the parking spot
    * @return returns true if a vehicle was successfully parked
    * @throws Exception as following:
    * if vehicle can't park for spot is taken, throws UnavailableSpotException;
    * if vehicle can't park for disability type not matching spot, throws InvalidSpotTypeException;
    * if vehicle can't park for size not matching spot, throws InvalidSpotSizeException;
    */
-  Boolean isParkingValid(Vehicle vehicle)
+
+  Boolean isParkingValid(AbstractVehicle vehicle, ParkingSpot parkingSpot)
       throws InvalidSpotSizeException, InvalidSpotTypeException, UnavailableSpotException;
 }
--- Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/ParkingSpot.java
+++ Assignment4/src/main/java/edu/neu/ccs/cs5004/problem1/ParkingSpot.java
@@ -6,8 +6,6 @@ public class ParkingSpot implements IParkingSpot {
   private Size spotSize;
   private Disabilities spotDisType;
   private Availability spotAvailability;
-  private Vehicle vehicle;
-  private ParkingSpot parkingSpot;
 
   public ParkingSpot(Size spotSize, Disabilities spotDisType,
       Availability spotAvailability) {
@@ -34,44 +32,32 @@ public class ParkingSpot implements IParkingSpot {
    * @return returns true if a vehicle was successfully parked, and false otherwise
    */
   @Override
-  public Boolean parkVehicle(Vehicle vehicle, ParkingSpot parkingSpot) {
-    this.parkingSpot = parkingSpot;
-    try {
-      if (parkingSpot.isParkingValid(vehicle)) {
-        this.parkingSpot = new ParkingSpot(this.spotSize, this.spotDisType, Availability.TAKEN);
-        return true;
-      }
-    } catch (InvalidSpotSizeException e) {
-      e.printStackTrace();
-    } catch (InvalidSpotTypeException e) {
-      e.printStackTrace();
-    } catch (UnavailableSpotException e) {
-      e.printStackTrace();
-    }
-    return false;
+  public Boolean parkVehicle(AbstractVehicle vehicle, IParkingSpot parkingSpot) {
+    return null;
   }
 
   /**
    * @param vehicle the vehicle which is looking for parking spot
-   * @return true if a vehicle was successfully parked
-   * @throws UnavailableSpotException if vehicle can't park for spot is taken
-   * @throws InvalidSpotTypeException if vehicle can't park for disability type not matching spot
-   * @throws InvalidSpotSizeException if vehicle can't park for size not matching spot
+   * @param parkingSpot the parking spot
+   * @return returns true if a vehicle was successfully parked
+   * @throws Exception as following: if vehicle can't park for spot is taken, throws
+   * UnavailableSpotException; if vehicle can't park for disability type not matching spot, throws
+   * InvalidSpotTypeException; if vehicle can't park for size not matching spot, throws
+   * InvalidSpotSizeException;
    */
   @Override
-  public Boolean isParkingValid(Vehicle vehicle)
+  public Boolean isParkingValid(AbstractVehicle vehicle, ParkingSpot parkingSpot)
       throws InvalidSpotSizeException, InvalidSpotTypeException, UnavailableSpotException {
-    if (vehicle.getVehicleSize().ordinal() > this.spotSize.ordinal()) {
+    if (vehicle.getVehicleSize().ordinal() > parkingSpot.getSpotSize().ordinal()) {
       throw new InvalidSpotSizeException(String.format("%s vehicle can't park in %s spot.",
-          vehicle.getVehicleSize().toString(), this.spotSize.toString()));
-    } else if (vehicle.getVehicleDisType().ordinal() > this.spotDisType.ordinal()) {
+          vehicle.getVehicleSize().toString(), parkingSpot.getSpotSize().toString()));
+    } else if (vehicle.getVehicleDisType().ordinal() > parkingSpot.getSpotDisType().ordinal()) {
       throw new InvalidSpotTypeException(String.format("%s vehicle can't park in %s spot.",
-          vehicle.getVehicleDisType().toString(), this.spotDisType.toString()));
-    } else if (this.spotAvailability.equals(Availability.TAKEN)) {
+          vehicle.getVehicleDisType().toString(), parkingSpot.getSpotDisType().toString()));
+    } else if (parkingSpot.getSpotAvailability().equals(Availability.TAKEN)) {
       throw new UnavailableSpotException(String.format("This spot is %s.",
-          this.spotAvailability.toString()));
-    } else {
-      return true;
+          parkingSpot.getSpotAvailability().toString()));
     }
+  return true;
   }
 }
