24388ac82782f330a2b573d3ab6addb446480ac8
Roy Chen <chen.rongy@husky.neu.edu>
2019-03-19 20:09:12 -0700

    update lab6
    add test for prob2

--- lab6/src/main/java/edu/neu/ccs/cs5004/prob1/FleetManager.java
+++ lab6/src/main/java/edu/neu/ccs/cs5004/prob1/FleetManager.java
@@ -2,10 +2,34 @@ package edu.neu.ccs.cs5004.prob1;
 
 public abstract class FleetManager {
 
-  public static TripReport drive(float distance, Vehicle vehicle) {
+  public static TripReport drive(Float distance, Vehicle vehicle) {
     Float speed = vehicle.getAveSpeed();
     Integer duration = Math.round(distance/speed);
     TripReport newReport = new TripReport(vehicle,speed, distance, duration);
+    return newReport;
+  }
+
+  public static TripReport drive(Float distance, Float speed, Float speedLimit, String id) {
+    Vehicle vehicle = new Vehicle(id, speed, speedLimit);
+    Integer duration;
+    if (speed > speedLimit) {
+      duration = Math.round(distance/speedLimit);
+    } else {
+      duration = Math.round(distance/speed);
+    }
+    TripReport newReport = new TripReport(vehicle,speed,distance,duration);
+    return newReport;
+  }
+
+  public static TripReport drive(Integer duration, Float distance, String id, Float speedLimit) {
+    Float speed;
+    if (distance/duration>speedLimit) {
+      speed = speedLimit;
+    } else {
+      speed = distance/duration;
+    }
+    Vehicle vehicle = new Vehicle(id, speed, speedLimit);
+    TripReport newReport = new TripReport(vehicle,speed,distance, duration);
     return newReport;
   }
 
--- lab6/src/test/java/edu/neu/ccs/cs5004/prob1/FleetManagerTest.java
+++ lab6/src/test/java/edu/neu/ccs/cs5004/prob1/FleetManagerTest.java
@@ -20,11 +20,35 @@ public class FleetManagerTest {
   }
 
   @Test
-  public void testDrive() {
+  public void testDrive1() {
     TripReport newReport = FleetManager.drive(dist, testCar);
     assertEquals(testCar, newReport.getVehicle());
     assertEquals(dist, newReport.getDist());
     assertEquals(aveSpeed, newReport.getSpeedMilePerMin());
     assertEquals(new Integer(29), newReport.getDurationInMin());
+  }
+
+  @Test
+  public void testDrive2() {
+    TripReport newReport = FleetManager.drive(dist, aveSpeed, maxSpeed, "WBE2864");
+    assertEquals("WBE2864",newReport.getVehicle().getID());
+    assertEquals(aveSpeed,newReport.getVehicle().getAveSpeed());
+    assertEquals(maxSpeed,newReport.getVehicle().getMaxSpeed());
+    assertEquals(dist,newReport.getDist());
+    assertEquals(aveSpeed,newReport.getSpeedMilePerMin());
+    assertEquals(new Integer(29),newReport.getDurationInMin());
+
+  }
+
+
+  @Test
+  public void testDrive3() {
+    TripReport newReport = FleetManager.drive(29, dist, "OKM2837", maxSpeed);
+    assertEquals("OKM2837",newReport.getVehicle().getID());
+    assertEquals(Math.round(aveSpeed),Math.round(newReport.getVehicle().getAveSpeed()));
+    assertEquals(maxSpeed,newReport.getVehicle().getMaxSpeed());
+    assertEquals(Math.round(aveSpeed),Math.round(newReport.getSpeedMilePerMin()));
+    assertEquals(dist,newReport.getDist());
+    assertEquals(new Integer(29),newReport.getDurationInMin());
   }
 }
